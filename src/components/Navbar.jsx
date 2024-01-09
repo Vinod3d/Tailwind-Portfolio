d309c99fa63b030f744715d7b4c811638e9c65f9
Vinod Chandra <vinodchandra979@gmail.com>
2024-01-09 17:57:42 +0530

    tailwind portfolio

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import { links } from "../data"
+
+const Navbar = () => {
+  return (
+    <nav className='bg-emerald-100'>
+        <div className="align-element py-4 flex flex-col sm:flex-row sm:gap-x-16 sm:items-center sm:py-8">
+            <h3 className='text-3xl font-bold'>Web <span className='text-emerald-600'>Dev</span></h3>
+            <div className="flex gap-x-3">
+                {links.map((link)=>{
+                    const {id, text, href} = link;
+                    return(
+                        <a key={id} href={href} className="capitalize text-lg tracking-wider hover:text-emerald-600 duration-300">{text}</a>
+                    )
+                })}
+            </div>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ src/components/Skills.jsx
@@ -0,0 +1,19 @@
+import SectionTitle from './SectionTitle'
+import { skills } from '../data'
+import SkillsCard from './SkillsCard'
+
+const Skills = () => {
+    console.log(skills.text)
+  return (
+    <section className='py-20 align-element' id='skills'>
+        <SectionTitle text='tech stack'/>
+        <div className="py-16 grid gap-8 md:grid-cols-2 lg:grid-cols-3">
+            {skills.map((skills) => {
+                return <SkillsCard key={skills.id} {...skills}/>
+            })}
+        </div>
+    </section>
+  )
+}
+
+export default Skills
